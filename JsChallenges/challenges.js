6f2a57ce05ad334b51defc931d25bca30b1cb7dd
Mohamed Yusuf <Mohamed-yusuf2@hotmail.com>
2023-01-30 23:40:15 -0600

    challenge part 1

---
+++ JsChallenges/challenges.js
@@ -0,0 +1,25 @@
+//Given two arrays, create a function that lets a user know (true/false) whether
+//these two arrays contain common items
+// example 1
+// const array1 = ['a','b','c','x'];
+// const array2 = ['z','y','i'];
+// return false
+//---------------------------------------
+
+//example 2
+// const array1 = ['a','b','c','x'];
+// const array2 = ['z','y','x'];
+// should return true
+
+function containsCommonItems(array1,array2){
+    for(let i = 0; i < array1.length; i++){
+        for(let j = 0; j < array2.length; j++){
+            if( array1[i] === array2[j]){
+                return true
+            }
+            else{
+                return false
+            }
+        }
+    }
+}
