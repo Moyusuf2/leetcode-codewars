a772998adcdd76bcd6661e13612b15e1be05bfbc
Mohamed Yusuf <Mohamed-yusuf2@hotmail.com>
2023-01-31 19:15:39 -0600

    change code

--- JsChallenges/challenges.js
+++ JsChallenges/challenges.js
@@ -11,15 +11,18 @@
 // const array2 = ['z','y','x'];
 // should return true
 
+
+const array1 = ['a','b','c','x'];
+const array2 = ['z','y','x'];
 function containsCommonItems(array1,array2){
     for(let i = 0; i < array1.length; i++){
         for(let j = 0; j < array2.length; j++){
             if( array1[i] === array2[j]){
                 return true
             }
-            else{
-                return false
-            }
         }
     }
+    return false
 }
+
+containsCommonItems(array1,array2)
