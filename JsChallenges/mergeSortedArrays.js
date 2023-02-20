84efbd26fa0e400c1b74329d545971e621ebed05
Mohamed Yusuf <Mohamed-yusuf2@hotmail.com>
2023-02-20 11:42:45 -0600

    solution for valid Anagram

--- JsChallenges/mergeSortedArrays.js
+++ JsChallenges/mergeSortedArrays.js
@@ -2,7 +2,7 @@ let array1 = [0,3,4,31];
 let array2 = [4,6,30];
 
 function mergeArrays(arr1,arr2){
-    let newArray = arr1.concat(arr2)
+    let newArray = arr1.concat(arr2);
     let sorted = newArray.sort(function(a,b){return a-b});
     return sorted;
 }
