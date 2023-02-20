cb853999b81f1cb0c787f34c2dd4801099794e1a
Mohamed Yusuf <Mohamed-yusuf2@hotmail.com>
2023-02-19 23:40:29 -0600

    solution for merged arrays

---
+++ JsChallenges/mergeSortedArrays.js
@@ -0,0 +1,11 @@
+let array1 = [0,3,4,31];
+let array2 = [4,6,30];
+
+function mergeArrays(arr1,arr2){
+    let newArray = arr1.concat(arr2)
+    let sorted = newArray.sort(function(a,b){return a-b});
+    return sorted;
+}
+
+let result = mergeArrays(array1,array2);
+console.log(result);
